17e809aba4a97ad9b690d19666188b4bc70633d2
SebastianCervantes <42254216+SebastianCervantes@users.noreply.github.com>
2018-12-03 18:24:53 -0800

    Commit

---
+++ js/jquery.js
@@ -0,0 +1,28 @@
+$(function() {
+  var buttonpressed;
+  $('body button').bind('click', function() {
+    buttonpressed = $(this).attr('id');
+    if(buttonpressed=='equipos'){
+      //alert('Soy un click feliz de nombre '+buttonpressed);
+        $('.box').fadeOut(900,function() {
+          $('.box > img').attr('src', 'Images/Votos/equipo1.png');
+        }).fadeIn();
+    } else if (buttonpressed=='individuales') {
+      $('.box').fadeOut(900, function() {
+        $('.box > img').attr('src', 'Images/Votos/individuales1.png');
+      }).fadeIn();
+    } else if(buttonpressed=='parejas') {
+      $('.box').fadeOut(900, function() {
+        $('.box > img').attr('src', 'Images/Votos/parejas1.png');
+      }).fadeIn();
+    } else if (buttonpressed== 'close') {
+      $('.voto-overlay').fadeOut();
+    }
+ });
+
+  $('.votos img').bind('click', function(){
+    buttonpressed = $(this).attr('class');
+    if()
+  });
+
+});
